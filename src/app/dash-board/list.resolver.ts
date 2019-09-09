4810503ad8947e48d39e87dae23a979ace8e8b79
ope <opeoluwaalase@gmail.com>
2019-09-09 15:27:46 +0100

    fix
    dashboard resolver now returns an observable as expected

--- src/app/dash-board/list.resolver.ts
+++ src/app/dash-board/list.resolver.ts
@@ -1,14 +1,15 @@
 import {Injectable} from '@angular/core';
 import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from '@angular/router';
 import {ListService} from '../shared/list.service';
+import {Observable} from 'rxjs';
+import {tap} from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'})
 export class ListResolver implements Resolve<any> {
   constructor(private listServ: ListService) { }
 
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    this.listServ.fetchLists().subscribe(data => {  // fetchs data before page loads
-      this.listServ.listSubject.next(null);
-    });
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> {
+    return this.listServ.fetchLists().pipe(tap(data => {  // fetchs data before page loads
+      this.listServ.listSubject.next(null); }));
   }
 }
